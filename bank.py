3fd8c2fa860fa479bd4f14d8957d24c0021df696
BotosBence <botosbence01@gmail.com>
2023-11-15 10:20:12 +0100

    save

---
+++ bank.py
@@ -0,0 +1,13 @@
+class bank():
+    bankaccount_data = [] #
+
+    def bank_up(self):
+      pass
+    def bankaccount_up(self):
+      pass
+    def bankaccount_list(self):
+      pass
+    def bankaccount_del(self):
+      pass
+    def user_up(self):
+      
