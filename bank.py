bf3b34dfe324fd367a579c4d8176d69620e9140e
BotosBence <botosbence01@gmail.com>
2023-11-15 10:45:50 +0100

    save

--- bank.py
+++ bank.py
@@ -1,17 +1,49 @@
 class bank():
-    bankaccount_data = [] #
+    banknames = [] # bankName, headquarters
+    bankoffices = [] # bankName, userAccounts
+    
+    
+    def __init__(self): # this defines lists
+      pass
+    def add_new_bank(self): # record new bank to bankname list
+      pass
+    def check_bank_name(self, banknames): # search for bank in bankname
+      pass
 
-    def bank_up(self):
+
+    def addBankOffices(self, banknames): # new bank office felvetele
       pass
-    def bankaccount_up(self):
+    def check_bank(self, bankname): # this function checks if bank doesn't exist in bankname and gives you the chance to record it
       pass
-    def bankaccount_list(self):
+    def bankoffice_del(self, bankoffice):
       pass
-    def bankaccount_del(self):
-      pass
-    def user_up(self):
-      pass
-    def user_list(self):
-      pass
-    def user_del(self):
-      pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def bank_up(self): # 
+    #   pass
+    # def bankaccount_up(self):
+    #   pass
+    # def bankaccount_list(self):
+    #   pass
+    # def bankaccount_del(self):
+    #   pass
+    # def user_up(self):
+    #   pass
+    # def user_list(self):
+    #   pass
+    # def user_del(self):
+    #   pass
