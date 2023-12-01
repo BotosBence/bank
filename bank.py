e9b1bb3c1b05a854666b8ff7e973bbc94e993d92
BotosBence <botosbence01@gmail.com>
2023-12-01 13:26:07 +0100

    update bank / still doesn't work

--- bank.py
+++ bank.py
@@ -1,44 +1,74 @@
+def add(self, key, value):  # this just adds a key and a value to a dictonary
+    self[key] = value
+
+
 class bank():
-    def add(self, key, value):  # this just adds a key and a value to a dictonary
-        self[key] = value
-
     def __init__(self):  # this defines lists
-        self.banknames = {}  # bankName, headquarters
-        self.employees = {}  # bankName, userAccounts - this is (probably) where tech support is
+        self.banknames = {}  # bankNames, employees list, customer list
+        self.employees = {}  # employeeName, bankName Where employee is working - this is (probably) where tech support is
 
-    def add_new_bank(self):  # record new bank to bankname list
-        user_input = input("Name of the bank: ")
-        if user_input is not None:
-            self.banknames.add(user_input, None)
-        else:
-            return False
+    def add_new_bank(self, user_input = None):  # record new bank to bankname list
+        while user_input is None:
+            user_input = input("Name of the bank: ")
+            if user_input not in self.banknames:
+                self.banknames.add(user_input, None)
+            else:
+                user_input = None
+
+    def rename_bank(self, user_input = None):  # record new bank to bankname list
+        while user_input is None:
+            user_input = input("Name of the bank: ")
+            if user_input not in self.banknames:
+                self.banknames.add(user_input, None)
+            else:
+                user_input = None
 
     def check_bank_name(self):  # search for bank in bankname
         user_input = input("Type in the name of the bank you're searching for: ")
         return True if user_input in self.banknames else False, user_input
+
     def check_employee_name(self):  # search for employee in employees
         user_input = input("Type in the name of the employee you're searching for: ")
-        return True if user_input in self.employees else False, user_input
+        return True if user_input in self.employees.keys() else False, user_input
 
-    def addEmployee(self):  # new employee recording
-        user_input = input("Name of the employee: ")
-        if user_input is not None:
-            self.employees.add(user_input, None)
-        else:
-            return False
+    def addEmployee(self, user_input = None):  # new employee recording
+        while user_input is None:
+            user_input = input("Name of the bank where you want to record the employee: ")
+            if user_input in self.banknames:
+                bank = user_input
+            else:
+                user_input = None
+        while user_input is None:
+            user_input = input("Name of the employee: ")
+            if user_input not in self.employees:
+                self.employees.add(user_input, bank)
+            else:
+                user_input = None
 
-    def smart_check_bank(self, get, user_input):  # function checks if bank doesn't exist in bankname and gives you the
+    def smart_check_bank(self, get = True, user_input = None):  # function checks if bank doesn't exist in bankname and gives you the
         # chance to record it
-        "There is a bank with that name" if get is True else "there are no banks with that name"
         while get:
             question = input("Would you like to record a new bank?[y/n]: ")
-            if question:
+            if question.lower() is "y":
                 self.banknames.add(user_input, None)
+            else:
+                question = input("Are you sure?[y/n]: ")
+                if question.lower() is "y":
+                    get = False
 
-    def smart_bankoffice_del(self):  # searches for and deletes bank office, it's smart because it searches
 
+
+    def smart_bankoffice_del(self, get = True, user_input = None):  # searches for and deletes bank office, it's smart because it searches
+        while get:
+            question = input("Would you like to record a new bank?[y/n]: ")
+            if question.lower() is "y":
+                self.banknames.add(user_input, None)
+            else:
+                question = input("Are you sure?[y/n]: ")
+                if question.lower() is "y":
+                    get = False
     def bankoffice_list(self):  # lists bankoffices
-        print(self.employees)
+        print(self.employees.keys())
 
     def save(self):  # probably doesn't work
         f = open('save.txt', 'r')
@@ -54,10 +84,10 @@ class bank():
 
 class customer():
     def __init__(self):  # this function was made for list_customer function
-        self.customers = []
+        self.customers = {}
 
-    def last_id(self):  # I don't know what this does or why this was created but it needs to be called
-        return
+    def last_id(self):  # I don't know what this does or why this was created but it needs to be called in self.customers
+        pass
 
     def add_customer(self):  # add new customer to customer list
         user_input = None
@@ -68,4 +98,3 @@ class customer():
 
     def list_customer(self):  # lists customers from self
         print(self.customers)
-
