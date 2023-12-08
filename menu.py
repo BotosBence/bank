a468fab99a3338fa9e8c0684b1a9f136c6a3d3a3
Botos Bence <botosbence01@gmail.com>
2023-12-08 19:21:16 +0100

    példák

--- menu.py
+++ menu.py
@@ -1,11 +1,13 @@
 from Bank_software import Bank, Customer
 
+bank = Bank()
 customer = Customer()
+
 customer.add_customer('Alice', 25, "Bank")
 customer.add_customer('Bob', 35, "Bank")
+customer.assign_bank(1, bank.bankname)
 customer.list_customers()
 
-bank = Bank()
 bank.add_new_bank('Bank')
 bank.add_employee('John Doe', 30, "Bank")
 
